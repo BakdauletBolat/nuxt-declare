e23ba5373cdd5c0c8c7c850b3665a5e56c162987
Lisa <bakosh21345@gmail.com>
2023-01-31 17:29:44 +0600

    vmaska added

--- components/ui/InputField.vue
+++ components/ui/InputField.vue
@@ -3,24 +3,15 @@
 
     <div class="relative">
       <div class="absolute z-10 b-placeholder top-1/2 -translate-y-1/2" :class="{
-                'b-placeholder--active': isFocus || value
-            }">{{ placeholder }}
+        'b-placeholder--active': isFocus || value
+      }">{{ placeholder }}
       </div>
-      <input @input="onInput($event)" v-maska
-             :data-maska="isPhone ? '+7 (###) ### ## ##' : ''"
-             autocomplete="off"
-             :aria-autocomplete="false"
-             :value="value"
-             :disabled="disabled"
-             min="0"
-             max="5000000"
-             @focusin="isFocus = true"
-             :type="isOpen ? 'text' : type"
-             @focusout="isFocus = false"
-             class="b-input w-full py-[8px] border-b border-b-[#878787]"
-             :name="name"/>
+      <input @input="onInput($event)" v-maska :data-maska="isPhone ? '+7 (###) ### ## ##' : ''" autocomplete="off"
+        :aria-autocomplete="false" :value="value" :disabled="disabled" min="0" max="5000000" @focusin="isFocus = true"
+        :type="isOpen ? 'text' : type" @focusout="isFocus = false"
+        class="b-input w-full py-[8px] border-b border-b-[#878787]" :name="name" />
       <img alt="isOPen" v-if="type === 'password'" @click="isOpen = !isOpen"
-           class="cursor-pointer absolute right-0 top-1/2 -translate-y-1/2" :src="isOpen ? CloseImg : OpenImg"/>
+        class="cursor-pointer absolute right-0 top-1/2 -translate-y-1/2" :src="isOpen ? CloseImg : OpenImg" />
     </div>
     <div v-if="errorMessage" class="mt-[10px] text-red-500">
       {{ errorMessage }}
@@ -30,10 +21,10 @@
 
 </template>
 <script setup lang="ts">
-import {ref} from 'vue';
+import { ref } from 'vue';
 import CloseImg from '@/assets/icons/close.png';
 import OpenImg from '@/assets/icons/open.png';
-
+import { vMaska } from 'maska';
 defineProps(['name', 'disabled', 'placeholder', 'value', 'errorMessage', 'type', 'isPhone']);
 const emit = defineEmits(['update:value']);
 const onInput = (event: any) => {
