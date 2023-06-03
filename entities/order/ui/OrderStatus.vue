22b5eee7600285651f44da1681040bbd801b67da
Bolat <bakosh1345@gmail.com>
2023-06-03 19:12:21 +0600

    fix(front): Babur fixes

---
+++ entities/order/ui/OrderStatus.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="lowercase" :style="{
+                    color: get_color(status)
+    }">{{ status.name }}</div>
+</template>
+<script lang="ts" setup>
+import { IStatus } from '../model/interface';
+
+const props = defineProps<{
+    status: IStatus
+}>();
+
+
+const get_color = (status: IStatus) => {
+    switch (status.alias) {
+        case 'NEW':
+            return '#1E90FF'
+    }
+}
+
+
+</script>
---
+++ pages/forget-password/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <SafeArea>
+        
+    </SafeArea>
+</template>
+<script lang="ts" setup>
+</script>
